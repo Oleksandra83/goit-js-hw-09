2ccc1ae6459b6cbd1c5eeadd88918991f5b248d1
Oleksandra Andrushchenko <oleksandraandrusenko6@gmail.com>
2023-04-16 11:52:15 +0300

    add task3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -28,5 +28,5 @@ function offChangeColor() {
 }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
